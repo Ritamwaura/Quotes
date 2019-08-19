8804f49b77f9652138e716c79990524b76b0dd7d
barakamulas <barakafrank18@gmail.com>
2019-08-19 07:32:47 +0300

    Redefine class Quote to include dateCreated and add a function in quotComponent.ts to add a new quote when the form is submitted

--- src/app/quot/quot.component.ts
+++ src/app/quot/quot.component.ts
@@ -9,12 +9,19 @@ export class QuotComponent implements OnInit {
   title = 'quotes to trust';
 
   quotes: Quote[]=[
-    new Quote(1,'Albert Einstein','I have no special talent. I am only passionately curious.'),
-    new Quote(2,'kkkkkkkk','llllllllll')
+    new Quote(1,'Albert Einstein','I have no special talent. I am only passionately curious.',new Date(2018,7,13)),
+    new Quote(2,'Rita Brittney','Love is powerful',new Date(2018,7,13))
   ];
 
   toggleDescription(index){
     this.quotes[index].showDescription=!this.quotes[index].showDescription;
+  }
+
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.dateCreated = new Date(quote.dateCreated)
+    this.quotes.push(quote)
   }
 
 
